fe2ed61c5e917234cf361cd072bfcac2aaff6043
Gautam.A <gautam.abhyankar@gmail.com>
2020-04-05 22:59:20 +0530

    Initial Commit

---
+++ RoWifi Alpha/Program.cs
@@ -0,0 +1,49 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using RoWifi_Alpha.Utilities;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RoWifi_Alpha
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            using (var services = ConfigureServices())
+            {
+                DiscordSocketClient client = services.GetRequiredService<DiscordSocketClient>();
+                client.Log += LogAsync;
+                services.GetRequiredService<CommandService>().Log += LogAsync;
+
+                await client.LoginAsync(TokenType.Bot, "");
+                await client.StartAsync();
+
+                await services.GetRequiredService<CommandHandler>().InitializeAsync();
+                await Task.Delay(-1);
+            }
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+
+        private ServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandler>()
+                .AddSingleton<HttpClient>()
+                .BuildServiceProvider();
+        }
+    }
+}
---
+++ RoWifi Alpha/Utilities/CommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace RoWifi_Alpha.Utilities
+{
+    public class CommandHandler
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
+        private readonly IServiceProvider _services;
+
+        public CommandHandler(IServiceProvider services)
+        {
+            _client = services.GetRequiredService<DiscordSocketClient>();
+            _commands = services.GetRequiredService<CommandService>();
+            _services = services;
+
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task HandleCommandAsync (SocketMessage rawMessage)
+        {
+            if (!(rawMessage is SocketUserMessage message)) return;
+
+            int argPos = 0;
+            if (!(message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos)) || message.Author.IsBot)
+                return;
+
+            SocketCommandContext context = new SocketCommandContext(_client, message);
+            await _commands.ExecuteAsync(context, argPos, _services);
+        }
+    }
+}
