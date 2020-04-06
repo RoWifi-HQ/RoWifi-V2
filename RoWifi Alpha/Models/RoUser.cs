f96c6ac0ab928f34bc5e6b767d184c51d71b4005
Gautam.A <gautam.abhyankar@gmail.com>
2020-04-06 15:28:44 +0530

    Add Model: RoUser

---
+++ RoWifi Alpha/Models/RoUser.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoWifi_Alpha.Models
+{
+    public class RoUser
+    {
+        [BsonId]
+        [BsonElement("DiscordId")]
+        public ulong DiscordId { get; set; }
+
+        [BsonElement("RobloxId")]
+        public int RobloxId { get; set; }
+    }
+}
