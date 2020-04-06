6a532b2330fe19441840436478538667faf75316
Gautam.A <gautam.abhyankar@gmail.com>
2020-04-07 01:52:51 +0530

    Added Module Exceptions

---
+++ RoWifi Alpha/Exceptions/RoMongoException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoWifi_Alpha.Exceptions
+{
+    public class RoMongoException : Exception
+    {
+        public RoMongoException() { }
+        public RoMongoException(string message) : base(message) { }
+        public RoMongoException(string message, Exception inner): base(message, inner) { }
+    }
+}
---
+++ RoWifi Alpha/Exceptions/RobloxException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoWifi_Alpha.Exceptions
+{
+    public class RobloxException : Exception
+    {
+        public RobloxException() { }
+        public RobloxException(string message) : base(message) { }
+        public RobloxException(string message, Exception inner) : base(message, inner) { }
+    }
+}
