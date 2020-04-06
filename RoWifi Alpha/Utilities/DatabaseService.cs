397a6c0bee299262e69bad9856acc7feb937bcbd
Gautam.A <gautam.abhyankar@gmail.com>
2020-04-06 15:29:34 +0530

    Added Modules: Database & Roblox

---
+++ RoWifi Alpha/Utilities/DatabaseService.cs
@@ -0,0 +1,60 @@
+﻿using MongoDB.Driver;
+using RoWifi_Alpha.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoWifi_Alpha.Utilities
+{
+    public class DatabaseService
+    {
+        private readonly IMongoClient _client;
+        private readonly IMongoDatabase _database;
+        private readonly IMongoCollection<RoUser> _users;
+
+        public DatabaseService()
+        {
+            _client = new MongoClient(Environment.GetEnvironmentVariable("DBConn"));
+            _database = _client.GetDatabase("RoWifi");
+            _users = _database.GetCollection<RoUser>("users");
+        }
+
+        public async Task<RoUser> GetUserAsync(ulong DiscordId)
+        {
+            IEnumerable<RoUser> users = await GetUsersAsync(new List<ulong>() { DiscordId });
+            return users.FirstOrDefault();
+        }
+
+        public async Task<IEnumerable<RoUser>> GetUsersAsync(IEnumerable<ulong> DiscordIds)
+        {
+            try
+            {
+                FilterDefinition<RoUser> filter = Builders<RoUser>.Filter.In(u => u.DiscordId, DiscordIds);
+                IAsyncCursor<RoUser> cursor = await _users.FindAsync(filter);
+                return cursor.ToEnumerable();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> AddUser(RoUser user, bool newUser = true)
+        {
+            try
+            {
+                if (newUser)
+                    await _users.InsertOneAsync(user);
+                else
+                    await _users.ReplaceOneAsync(u => u.DiscordId == user.DiscordId, user);
+                return true;
+            }
+            catch (Exception)
+            {
+                //Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ RoWifi Alpha/Utilities/RobloxService.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoWifi_Alpha.Utilities
+{
+    public class RobloxService
+    {
+        private readonly HttpClient _client;
+
+        public RobloxService(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<int?> GetIdFromUsername(string Username)
+        {
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync(new Uri($"http://api.roblox.com/users/get-by-username?username={Username}"));
+                string res = await response.Content.ReadAsStringAsync();
+                JObject obj = JObject.Parse(res);
+                return (int?)obj["Id"];
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> CheckCode(int RobloxId, string code)
+        {
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync(new Uri($"https://www.roblox.com/users/{RobloxId}/profile"));
+                string res = await response.Content.ReadAsStringAsync();
+                bool IsPresent = res.Contains(code, StringComparison.OrdinalIgnoreCase);
+                return IsPresent;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
