cc3a259690e6537f5eecf278c32e6d3fb2495c98
Gautam.A <gautam.abhyankar@gmail.com>
2020-04-06 15:30:41 +0530

    Added Miscellanous Methods and Custom Command Result

---
+++ RoWifi Alpha/Utilities/Miscellanous.cs
@@ -0,0 +1,31 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoWifi_Alpha.Utilities
+{
+    public static class Miscellanous
+    {
+        private static readonly string[] Codes = new string[] { "cat", "dog", "sun", "rain", "snow", "alcazar", "dight", "night", "morning", "eyewater", "flaws", "physics", "chemistry", "history", "martlet", "nagware", "coffee", "tea", "red", "blue", "green", "orange", "pink" };
+        public static string GenerateCode()
+        {
+            Random rand = new Random();
+            string code1 = Codes[rand.Next(0, Codes.Length)];
+            string code2 = Codes[rand.Next(0, Codes.Length)];
+            string code3 = Codes[rand.Next(0, Codes.Length)];
+            return code1 + " " + code2 + " " + code3;
+        }
+
+        public static EmbedBuilder GetDefaultEmbed()
+        {
+            EmbedBuilder embed = new EmbedBuilder
+            {
+                Timestamp = DateTime.Now,
+                Color = Color.Blue,
+            };
+            embed.WithFooter("RoWifi");
+            return embed;
+        }
+    }
+}
---
+++ RoWifi Alpha/Utilities/RoWifiResult.cs
@@ -0,0 +1,25 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoWifi_Alpha.Utilities
+{
+    public class RoWifiResult : RuntimeResult
+    {
+        public RoWifiResult(CommandError? error, string reason) : base(error, reason)
+        {
+        }
+
+        public static RoWifiResult FromError(string reason) =>
+            new RoWifiResult(CommandError.Unsuccessful, reason);
+        public static RoWifiResult FromSuccess(string reason = null) =>
+            new RoWifiResult(null, reason);
+
+        public static RoWifiResult FromRobloxError() =>
+            new RoWifiResult(CommandError.Exception, "There seems to be an error while interacting with the Roblox API. Please try again in a few minutes");
+
+        public static RoWifiResult FromMongoError() =>
+            new RoWifiResult(CommandError.Exception, "There was a problem in saving to the database. Pleas contact @Gautam.A#9539 immediately.");
+    }
+}
